ffa0a598847ff958c300a456ae8b2c8990b117e1
Marcus Lowe <marcus@create.xyz>
2024-03-11 12:04:47 -0700

    fix: build

--- packages/openai/tsup.config.ts
+++ packages/openai/tsup.config.ts
@@ -2,9 +2,9 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
   clean: true,
-  entry: ["./src/tag.ts"],
+  entry: ["./src/index.ts"],
   dts: {
-    entry: ["./src/tag.ts", "./src/types.ts"],
+    entry: ["./src/index.ts"],
   },
   format: ["cjs", "esm"],
   sourcemap: false,
